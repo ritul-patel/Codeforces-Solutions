04ed89fd5ab8c1b7fb1bbbbaa3c776927dfc94bd
Ritul Patel <143929587+ritul-01@users.noreply.github.com>
2024-02-20 17:35:13 +0530

    Create 1916A.cpp

---
+++ Codeforces/1916A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+ 
+int main() {
+    int t;
+    cin >> t;
+    forn(tt, t) {
+        string s;
+        cin >> s;
+        bool ok = true;
+        if (s.length() % 2 == 0) {
+            forn(i, s.length() / 2)
+                if (s[i] != s[i + s.length() / 2])
+                    ok = false;
+        } else
+            ok = false;
+        cout << (ok ? "YES" : "NO") << endl;
+    }
+}
