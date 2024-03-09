53c6e1e7dfc0415c6032d1ee25b1c9c9e874f65c
Ritul Patel <143929587+ritul-01@users.noreply.github.com>
2024-03-09 17:35:05 +0530

    Solution of problem : 1921C - Sending Messages.cpp

---
+++ Codeforces Solution/1921C - Sending Messages.cpp
@@ -0,0 +1,77 @@
+//                  #250345860	Mar/09/2024 17:33UTC+5.5	Ritul_Patel	1921C - Sending Messages	C++20 (GCC 11-64)	Accepted	31 ms	4700 KB
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+
+#define ll long long
+#define ld long double
+#define Go ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(NULL)
+
+// Loops
+#define fi(l, r) for (ll i = l; i < r; i++)
+#define fj(l, r) for (ll j = l; j < r; j++)
+#define fk(l, r) for (ll k = l; k < r; k++)
+#define fir(r, l) for (ll i = r; i >= l; i--)
+#define fjr(r, l) for (ll j = r; j >= l; j--)
+#define rfo(i,a,b)  for(ll i=a;i>=b;i--)
+#define fq(q) while(q--)
+
+// Inputs
+#define in(n) ll n;cin>>n;
+#define inn(n,k) ll n,k;cin>>n>>k;
+#define strin(s) string s;cin>>s;
+// Outputs
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+
+// STL
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+
+// Sorting
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define rev(v) reverse(all(v));
+#define maxa(v) *max_element(v.begin(),v.end())
+#define mina(v) *min_element(v.begin(),v.end())
+
+
+// Test Case
+#define Test_case \
+    int t = 1;    \
+    cin >> t;     \
+    while (t--)
+
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+
+void solve() {
+    ll n,f,a,b;
+    cin >> n >> f >> a >> b;
+    vector<ll>v(n);
+    for(auto &i:v) cin >> i;
+    v.insert(v.begin(), 0);
+    for(int i = 1 ; i <= n ; ++i ){
+        ll x = (v[i]-v[i-1])*a;
+        f -= min(x,b);
+        if(f<=0)
+        {
+            no;
+            return;
+        }
+    }
+    yes;
+}
+
+int main() {
+    Go;
+    Test_case {
+        solve();
+    }
+
+    return 0;
+}
