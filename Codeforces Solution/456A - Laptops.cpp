98a552896dc212afbfaf1c9ea319ae6783163610
Ritul Patel <143929587+ritul-01@users.noreply.github.com>
2024-03-30 18:02:45 +0530

    Solution of problem : 456A - Laptops,cpp

---
+++ Codeforces Solution/456A - Laptops.cpp
@@ -0,0 +1,67 @@
+//254101552	Mar/30/2024 17:59UTC+5.5	Ritul_Patel	456A - Laptops	C++20 (GCC 13-64)	Accepted	187 ms	100 KB
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+
+#define ll long long
+#define ld long double
+#define Go ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(NULL)
+
+// Loops
+#define fi(l, r) for (ll i = l; i < r; i++)
+#define fj(l, r) for (ll j = l; j < r; j++)
+#define fk(l, r) for (ll k = l; k < r; k++)
+#define fir(r, l) for (ll i = r; i >= l; i--)
+#define fjr(r, l) for (ll j = r; j >= l; j--)
+#define rfo(i,a,b)  for(ll i=a;i>=b;i--)
+#define fq(q) while(q--)
+
+// Inputs
+#define in(n) ll n;cin>>n;
+#define inn(n,k) ll n,k;cin>>n>>k;
+#define strin(s) string s;cin>>s;
+// Outputs
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+
+// STL
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+
+// Sorting
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define rev(v) reverse(all(v));
+#define maxa(v) *max_element(v.begin(),v.end())
+#define mina(v) *min_element(v.begin(),v.end())
+
+
+// Test Case
+#define Test_case \
+    int t = 1;    \
+    cin >> t;     \
+    while (t--)
+
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+
+
+int main() {
+    int i, x, y, n;
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        cin >> x >> y;
+        if (x != y) {
+            cout << "Happy Alex" << endl;
+            return 0;
+        }
+    }
+    cout << "Poor Alex" << endl;
+    return 0;
+}
+
+
