1dec6db94f586ffe06840f1cbd7e4a000bcd5d13
Ritul Patel <143929587+202301086@users.noreply.github.com>
2024-04-14 13:22:27 +0530

    Solution of problem : 1890A - Doremy's Paint 3.cpp

---
+++ Codeforces Solution/1890A - Doremy's Paint 3.cpp
@@ -0,0 +1,94 @@
+//256607888	Apr/14/2024 13:03UTC+5.5	Ritul_Patel	1890A - Doremy's Paint 3	C++17 (GCC 7-32)	Accepted	46 ms	0 KB
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+
+#define ll long long
+#define ld long double
+#define Go ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(NULL)
+
+// Loops
+#define fi(l, r) for (ll i = l; i <= r; i++)
+#define fj(l, r) for (ll j = l; j < r; j++)
+#define fk(l, r) for (ll k = l; k < r; k++)
+#define fir(r, l) for (ll i = r; i >= l; i--)
+#define fjr(r, l) for (ll j = r; j >= l; j--)
+#define rfo(i,a,b)  for(ll i=a;i>=b;i--)
+#define fq(q) while(q--)
+
+// Inputs
+#define in(n) ll n;cin>>n;
+#define inn(n,k) ll n,k;cin>>n>>k;
+#define strin(s) string s;cin>>s;
+// Outputs
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+
+// STL
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+
+// Sorting
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define rev(v) reverse(all(v));
+#define maxa(v) *max_element(v.begin(),v.end())
+#define mina(v) *min_element(v.begin(),v.end())
+
+
+// Test Case
+#define Test_case \
+    int t = 1;    \
+    cin >> t;     \
+    while (t--)
+
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+//----------------------------&&&---------------------------------------------------------&&&----------------------------------\\
+
+void solve() {
+    int n;
+    cin >> n;
+   unordered_map<int, int> mp;
+    fi(1,n){
+        int x;
+        cin >> x;
+        mp[x]++;
+    }
+    
+    if(mp.size() == 1){
+        yes;
+        return;
+    }
+    else if(mp.size() > 2){
+        no;
+        return;
+    }
+    else{
+        auto it = mp.begin();
+        int a = it->S;
+        it++;
+        int b = it->S;
+        
+        if(a==b || abs(a-b) == 1){
+            yes;
+            return;
+        }
+        else{
+            no;
+            return;
+        }
+    }
+
+}
+int main() {
+    Go;
+    Test_case {
+        solve();
+    }
+
+    return 0;
+}
